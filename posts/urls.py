8378927007f249b85028a9ea39b1e8d401544432
wizardcalidad <yusufqoyum01@gmail.com>
2021-05-07 10:06:35 +0100

    Commiting to github straight up after the project

---
+++ posts/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+# from .views import PostList, PostDetail, UserList, UserDetail
+from rest_framework.routers import SimpleRouter
+from .views import UserViewSet, PostViewSet
+
+router = SimpleRouter()
+
+router.register('users', UserViewSet, basename='user')
+router.register('', PostViewSet, basename='posts')
+urlpatterns = router.urls
+
+# urlpatterns = [
+#     path('users/', UserList.as_view()),
+#     path('users/<int:pk>/', UserDetail.as_view()),
+#     path('<int:pk>/', PostDetail.as_view()),
+#     path('', PostList.as_view()),
+# ]
---
+++ posts/views.py
@@ -0,0 +1,35 @@
+from rest_framework import generics, viewsets
+from .permissions import IsAuthorOrReadOnly
+from .models import Post
+from django.contrib.auth import get_user_model
+from .serializers import PostSerializer, UserSerializer
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = (IsAuthorOrReadOnly, )
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+# class PostList(generics.ListCreateAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#
+#
+# class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+#     permission_classes = (IsAuthorOrReadOnly, )
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+#
+#
+# class UserList(generics.ListCreateAPIView):
+#     queryset = get_user_model().objects.all()
+#     serializer_class = UserSerializer
+#
+#
+# class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = get_user_model().objects.all()
+#     serializer_class = UserSerializer
